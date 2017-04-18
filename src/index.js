581474a13486f16f66ef7da88c408fe4493eba72
Mauro Junior <mauropereirajr@gmail.com>
2017-04-18 18:06:24 -0300

    chore(fixing api):

--- src/index.js
+++ src/index.js
@@ -8,9 +8,40 @@ module.exports = Object.freeze(Emitter);
 
 
 function Emitter() {
+  let listeners = [];
+
   return Object.freeze({
     on: noop,
     once: noop,
     off: noop
   });
+
+  function addListener(event, fn) {
+    listeners.push({ event: RouteParser(event), fn });
+  }
+
+  function once(event, fn) {
+    const onceFn = (...args) => {
+      fn(...args);
+      removeListener(event, onceFn);
+    };
+
+    addListener(event, onceFn);
+  }
+
+  function removeListener(event, fn) {
+    listeners = listeners.filter(ltr => ltr.event !== event || !fn || ltr.fn !== fn);
+  }
+
+  function removeAllListeners() {
+    listeners = [];
+  }
+
+  function emit(event, ...args) {
+    listeners.forEach(({ router, fn }) => {
+      if (router.match(event) !== false) {
+        fn(...result.concat(args));
+      }
+    });
+  }
 }
